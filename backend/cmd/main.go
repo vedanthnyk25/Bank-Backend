bc09806ec8ed7b997403e54df9b9af2a9f8508f0
vedanthnyk25 <vedanthnayak25@gmail.com>
2025-06-19 23:12:33 +0530

    Implement main application with graceful shutdown and database connection

---
+++ backend/cmd/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"backend/internal/db"
+	"context"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+// Handle graceful shutdown
+func main() {
+	db.ConnectDB()
+
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Welcome to the backend API!",
+		})
+	})
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("ListenAndServe(): %s\n", err)
+		}
+	}()
+	log.Println("Server Started on :8080")
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+
+	<-quit
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown: %s", err)
+	}
+
+	db.CloseDB()
+	log.Println("Server exiting")
+}
